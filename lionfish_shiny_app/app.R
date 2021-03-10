f3d4750b80092099fa68b6cea965346758f53d7d
anakunz <anastasiakunz@gmail.com>
2021-03-10 10:02:42 -0800

    fixed spatial :)

--- lionfish_shiny_app/app.R
+++ lionfish_shiny_app/app.R
@@ -15,6 +15,7 @@ library(here)
 library(janitor)
 library(tmap)
 library(sf)
+library(sp)
 
 tmap_mode("view")
 
@@ -31,7 +32,7 @@ my_bs_theme <- bs_theme(
 lionfish <- read_csv(here("lionfish_shiny_app", "lionfish_data.csv")) %>% 
     clean_names() %>% 
     mutate(common_name = case_when(
-        item_genus_species == "unid" ~ "Unknown", # CHECK THIS???
+        item_genus_species == "unid" ~ "Unknown", 
         item_genus_species == "Pterois_volaitans" ~ "Red Lionfish",
         item_genus_species == "Lysmata_sp" ~ "Peppermint Shrimp",
         item_genus_species == "Mysidium_spp" ~ "Mysid Shrimp", #check
@@ -52,6 +53,37 @@ lionfish <- read_csv(here("lionfish_shiny_app", "lionfish_data.csv")) %>%
         item_genus_species == "Thalassoma_bifasciatum" ~ "Blueheaded Wrasse",
         item_genus_species == "Liopropoma_carmabi" ~ "Candy Basslet"
     )) %>% 
+  mutate(prey_info = case_when(
+    common_name == "Peppermint Shrimp" ~ "Peppermint shrimp are native to the Gulf of Mexico and are small shrimp that are aptly named for their red striped bodies. They are detritus feeders and typically eat decomposing material in the ocean.",
+    common_name == "Mysid Shrimp" ~ "These small crustaceans are found in a variety of ecosystems but primarily in the Gulf of Mexico. Their two antennae and big eyes make them easy to recognize, and they feed mainly on algae and other detritus.",
+    common_name == "Cardinalfish" ~ "Cardinalfish are in the family ‘Apogonidae’ and are typically found in shallow tropical reefs. They often find refuge inside conch shells,and typically feed at night on benthic crustaceans and other small invertebrates. These small fish have two dorsal fins and large eyes and mouths that makes them recognizable.",
+    common_name == "Saddle Blenny" ~ "The Saddle Blenny is typically found among shallow waters in rocky and coral reefs throughout the Caribbean.  Colors vary, but many are red and brown with distinct stripes. These fish feed on small organisms such as benthic worms, shrimp and crabs."))
+
+# Read in the data for spatial use
+lionfish_spatial <- st_read(here("lionfish_shiny_app", "lionfish_data.csv")) %>% 
+  clean_names() %>% 
+  mutate(common_name = case_when(
+    item_genus_species == "unid" ~ "Unknown", 
+    item_genus_species == "Pterois_volaitans" ~ "Red Lionfish",
+    item_genus_species == "Lysmata_sp" ~ "Peppermint Shrimp",
+    item_genus_species == "Mysidium_spp" ~ "Mysid Shrimp", #check
+    item_genus_species == "Mycteroperca_venenosa" ~ "Yellofin grouper",
+    item_genus_species == "Gobiosoma_prochilos" ~ "Gobiosoma prochilos", #couldnt find it
+    item_genus_species == "Gonodactylus_smithii" ~ "Purple Spot Manits Shrimp",
+    item_genus_species == "Canthigaster_rostrata"~ "Sharpnose Puffer",
+    item_genus_species == "empty" ~ "Empty",
+    item_genus_species == "Cryptotomus_roseus" ~ "Bluelip Parrotfish",
+    item_genus_species == "Ophioblennius_atlanticus" ~ "Redlip Blenny",
+    item_genus_species == "Chromis_multilineata" ~ "Brown Chromis",
+    item_genus_species == "Stegastes_partitus" ~ "Bicolor Damselfish",
+    item_genus_species == "Gramma_loreto" ~ "Royal Gramma",
+    item_genus_species == "Callinectes_sapidus" ~ "Chesapeake Blue Crab",
+    item_genus_species == "Malacoctenus_trinagulatus" ~ "Saddle Blenny",
+    item_genus_species == "Apogon_spp." ~ "Cardinalfish",
+    item_genus_species == "Epinephelus_spp." ~ "Predatory Ray-Finned Fish", # better name???
+    item_genus_species == "Thalassoma_bifasciatum" ~ "Blueheaded Wrasse",
+    item_genus_species == "Liopropoma_carmabi" ~ "Candy Basslet"
+  )) %>% 
   mutate(prey_info = case_when(
     common_name == "Peppermint Shrimp" ~ "Peppermint shrimp are native to the Gulf of Mexico and are small shrimp that are aptly named for their red striped bodies. They are detritus feeders and typically eat decomposing material in the ocean.",
     common_name == "Mysid Shrimp" ~ "These small crustaceans are found in a variety of ecosystems but primarily in the Gulf of Mexico. Their two antennae and big eyes make them easy to recognize, and they feed mainly on algae and other detritus.",
@@ -131,7 +163,7 @@ ui <- fluidPage(theme = my_bs_theme,
                                     sidebarLayout(
                                        sidebarPanel(radioButtons(inputId = "select_site", 
                                                                   label = h5("Select Site:"),
-                                                                  choices = unique(lionfish$location), 
+                                                                  choices = unique(lionfish_spatial$location), 
                                                                   selected = "Paraiso")),
                                   
                                         mainPanel("Observations of Lionfish by location", tmapOutput("location_plot"))
@@ -211,7 +243,7 @@ server <- function(input, output) {
 
    spatial_reactive <- reactive({
     
-      lionfish %>% 
+      lionfish_spatial %>% 
        select(latitude, longitude, location) %>% 
        count(location, latitude, longitude) %>% 
        st_as_sf(., coords=c("longitude", "latitude"), crs = 4483) %>% 
